8190029cb50d267637001883488b31dc0995afee
Mihir Gupta <mihir0699@gmail.com>
2021-08-18 13:17:34 +0530

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -10,6 +10,17 @@ const Home = () => {
   useEffect(() => {
     if (!navigator.onLine) alert("Connect to internet!");
   }, [navigator]);
+ 
+ useEffect(()=>{
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+ }, [])
   return (
     <VideoState>
       <div className="App" style={{ height: "100%", width: "100%" }}>
