3eb11fa97ec96c0543287d01397db378efbd02aa
Mihir Gupta <mihir0699@gmail.com>
2022-03-20 18:07:12 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useEffect} from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
@@ -6,13 +6,24 @@ import "antd/dist/antd.css";
 import "font-awesome/css/font-awesome.min.css";
 import Footer from "./components/Footer/Footer";
 const App = () => {
+  useEffect(()=>{
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  },[])
   return (
+    <>
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5366413451953973"
+     data-ad-slot="8917011428"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
     <Router>
       <Switch>
         <Route path="/" component={Home} />
       </Switch>
       <Footer />
     </Router>
+</>
   );
 };
 
